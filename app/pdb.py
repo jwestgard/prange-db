463dd105abb695a61a20a4d7476e280244c0e0ba
Joshua Westgard <jwestgard@mac.com>
2015-09-27 12:54:38 -0400

    add migrations, config.py, error response pages

--- app/pdb.py
+++ app/pdb.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
@@ -8,11 +10,14 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Shell
+from flask.ext.migrate import Migrate, MigrateCommand
 import psycopg2
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'postgresql://prangedb_admin:admin@localhost/prangedb'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
@@ -22,6 +27,7 @@ db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+migrate = Migrate(app, db)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
@@ -34,7 +40,7 @@ class Role(db.Model):
     users = db.relationship('User', backref='role')
     
     def __repr__(self):
-        return '<Role {0}>'.format(self.username)
+        return '<Role {0}>'.format(self.name)
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -45,18 +51,36 @@ class User(db.Model):
     def __repr__(self):
         return '<User {0}>'.format(self.username)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), 
+    return render_template('index.html', form = form, 
+        name = session.get('name'), known = session.get('known', False),
         current_time=datetime.utcnow())
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 @app.route('/user/<name>')
 def user(name):
